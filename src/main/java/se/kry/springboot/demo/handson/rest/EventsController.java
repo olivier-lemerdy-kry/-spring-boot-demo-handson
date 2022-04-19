20c1dc11917602b68a6eec02dbe839bca62017b8
Olivier Le Merdy <olivier.lemerdy@kry.se>
2022-04-19 10:14:09 +0200

    Cleanup

--- src/main/java/se/kry/springboot/demo/handson/rest/EventsController.java
+++ src/main/java/se/kry/springboot/demo/handson/rest/EventsController.java
@@ -1,11 +1,9 @@
 package se.kry.springboot.demo.handson.rest;
 
-import java.net.URI;
 import java.util.UUID;
 import javax.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 import se.kry.springboot.demo.handson.domain.EventCreationRequest;
@@ -33,7 +30,6 @@ public class EventsController {
   }
 
   @PostMapping
-  @ResponseStatus(HttpStatus.CREATED)
   ResponseEntity<EventResponse> createEvent(@Valid @RequestBody EventCreationRequest eventCreationRequest,
                                             UriComponentsBuilder builder) {
     var event = service.createEvent(eventCreationRequest);
